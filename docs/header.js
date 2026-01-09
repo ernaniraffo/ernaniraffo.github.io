3f58e6c54c915659f7d651732a349d5101ef3c3f
Ernani Raffo <ernani.raffo@icloud.com>
2026-01-09 07:20:03 -0800

    Add header to all pages

---
+++ docs/header.js
@@ -0,0 +1,13 @@
+const headerHTML = `
+<header>
+  <h1>
+    <a href="/index.html">Ernani Raffo</a>
+  </h1>
+  <nav>
+    <a href="/dev-log.html">Dev Logs</a>
+    <a href="/about.html">About</a>
+  </nav>
+</header>
+`;
+
+document.body.insertAdjacentHTML('afterbegin', headerHTML);

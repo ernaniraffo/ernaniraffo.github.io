3615714279e3d5962d0631a0444f128321aeabab
Ernani Raffo <ernani.raffo@icloud.com>
2026-01-10 15:47:32 -0800

    Add footer to all pages

---
+++ docs/footer.js
@@ -0,0 +1,23 @@
+const footerHTML = `
+    <div class="footer">
+      <div class="social-links">
+        <div class="icon-link">
+          <img src="/assets/img/icon/email-envelope-line-white-icon.svg"
+            alt="email-icon">
+          <a href="mailto:ernaniraffo@gmail.com">ernaniraffo@gmail.com</a>
+        </div>
+        <div class="icon-link">
+          <img src="/assets/img/icon/github-mark-white.svg"
+            alt="github-mark">
+          <a href="https://github.com/ernaniraffo">ernaniraffo</a>
+        </div>
+        <div class="icon-link">
+          <img src="/assets/img/icon/logo.svg"
+            alt="x-logo">
+          <a href="https://www.x.com/ernaniraffo/">ernaniraffo</a>
+        </div>
+      </div>
+    </div>
+`;
+
+document.body.insertAdjacentHTML('beforeend', footerHTML);
